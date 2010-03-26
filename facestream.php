b6749394e13fdfef67c2026472d7a83670d967df
blackphantom <blackphantom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-26 13:12:05 +0000

    trunk update
    git-svn-id: https://plugins.svn.wordpress.org/facestream/trunk@221834 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facestream.php
+++ facestream.php
@@ -27,4 +27,36 @@ function facestream_init() {
 
 add_action( 'bp_init', 'facestream_init' );
 
+##############################################
+##                                          ##
+##             wp_cron stuff                ##
+##                                          ##
+##############################################
+
+//on plugin activation register cron
+register_activation_hook(__FILE__, 'facestream_activation');
+add_action('facestream_cron_event', 'facestream_cron');
+
+
+function facestream_activation() {
+    wp_schedule_single_event(time()+300, 'facestream_cron_event');    
+}
+
+function facestream_cron() {
+
+    global $wpdb;
+    // get all usermeta with facebook authorisation
+    if(get_site_option('facestream_user_settings_syncbp')==0){
+        $user_metas = $wpdb->get_results($wpdb->prepare("SELECT * FROM $wpdb->usermeta WHERE meta_key='facestream_session_key';"));
+        if($user_metas){
+            foreach ($user_metas as $user_meta){
+                facestream_getFacebook($user_meta->user_id);
+            }
+        }        
+    }
+    
+    wp_schedule_single_event(time()+300, 'facestream_cron_event');    
+    
+}
+
 ?>
